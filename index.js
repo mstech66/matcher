9a2aebfe2f53acbac8bc0c099c970360855f3f55
Manthan <manthanmevada66@gmail.com>
2023-01-17 19:35:26 +0530

    Added Vercel Build Config for Backend Part 3

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const cors = require("cors");
-const bodyParser = require('body-parser');
-var user = require('./routes/users.js');
+const bodyParser = require("body-parser");
+var user = require("./routes/users.js");
 const port = process.env.PORT || 4200;
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/dist/profileCompare'));
+app.use(express.static(__dirname + "/dist/profileCompare"));
 
 app.listen(port, function () {
-    console.log("Server running");
+  console.log("Server running");
 });
 
-app.use('/', user); //mongo operations belong here!
-app.get('*', function(req,res,next){
-    res.send(path.join(__dirname + '/dist/profileCompare/index.html'));
+app.use("/", user); //mongo operations belong here!
+app.get("*", function (req, res, next) {
+  res.send(path.join(__dirname + "/dist/profileCompare/index.html"));
 });
+
+module.exports = app;
